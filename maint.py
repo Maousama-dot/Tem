f1fcae144b37c8d464ff837b7cff21772d6294b8
Maousama-dot <shivashubhamrajput@gmail.com>
2025-07-18 22:03:03 +0530

    Update maint.py

--- maint.py
+++ maint.py
@@ -1,12 +1,7 @@
-from telethon import TelegramClient, events
+from telethon events
 import asyncio
 import os
-
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-bot_token = os.environ.get("bot_token")
-
-bot = TelegramClient("bot", API_ID, API_HASH)
+from maouconfig import bot
 
 @bot.on(events.NewMessage(pattern="/start"))
 async def handler(event):
@@ -17,4 +12,8 @@ async def main():
     print("✅ Bot is running...")
     await bot.run_until_disconnected()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f"An error occurred: {e}") 
