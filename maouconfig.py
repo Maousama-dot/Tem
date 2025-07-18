a756a588b60be4ba4749ec035dc8d94552868583
Maousama-dot <shivashubhamrajput@gmail.com>
2025-07-18 22:06:38 +0530

    Update maouconfig.py

--- maouconfig.py
+++ maouconfig.py
@@ -1,7 +1,9 @@
 from telethon import TelegramClient
+import os
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 bot_token = os.environ.get("bot_token")
+my_id = os.environ.get("my_id")
 
 bot = TelegramClient("bot", API_ID, API_HASH)
